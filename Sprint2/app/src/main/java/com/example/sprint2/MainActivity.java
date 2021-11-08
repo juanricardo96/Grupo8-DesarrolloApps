ddbbb2804855db5ac702c16a41d1ac41ec1ef689
Juan <juan.rodriguez@o365.unab.edu.co>
2021-11-08 02:54:25 -0500

    Se realizan calculos de IMC

--- Sprint2/app/src/main/java/com/example/sprint2/MainActivity.java
+++ Sprint2/app/src/main/java/com/example/sprint2/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.sprint2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -15,5 +19,18 @@ public class MainActivity extends AppCompatActivity {
 
         EditText estatura = (EditText) findViewById(R.id.textEstatura);
         EditText peso = (EditText) findViewById(R.id.textPeso);
+        Button botonCalcular = (Button) findViewById(R.id.boton_calcular);
+        final TextView imc = (TextView) findViewById(R.id.textIMC);
+
+        botonCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*String estaturaF = estatura.getText().toString();
+                String pesoF = peso.getText().toString();*/
+                float estaturaF = new Float(estatura.getText().toString());
+                float pesoF = new Float(peso.getText().toString());
+                imc.setText(""+ pesoF/((estaturaF/100)*(estaturaF/100)));
+            }
+        });
     }
 }
