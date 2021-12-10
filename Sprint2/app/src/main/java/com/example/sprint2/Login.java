cdf1723d18a523abbef2ee1768313b5124baaa41
Juan <juan.rodriguez@o365.unab.edu.co>
2021-12-09 23:29:42 -0500

    Se agrego Layout login

---
+++ Sprint2/app/src/main/java/com/example/sprint2/Login.java
@@ -0,0 +1,26 @@
+package com.example.sprint2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        Button botonCalcular = (Button) findViewById(R.id.boton_login);
+
+        botonCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Login.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
